d2c5974a2be315e48faa757c5bcc90ee4dc7af5e
lokmannicholas <lokmannicholas@gmail.com>
2016-09-09 22:09:29 +0800

    update install table

--- asiapay/app/code/local/Lokman/AsiaPay/sql/asiapay_setup/mysql4-install-0.1.0.php
+++ asiapay/app/code/local/Lokman/AsiaPay/sql/asiapay_setup/mysql4-install-0.1.0.php
@@ -2,9 +2,44 @@
 
 $installer = $this;
 $installer->startSetup();
-$sql=<<<SQLTEXT
-DROP TABLE IF EXISTS `asiapay_ref`;
-CREATE TABLE `asiapay_ref` (
+
+
+//
+//
+//$sql=<<<SQLTEXT
+//DROP TABLE IF EXISTS `asiapay_ref`;
+//CREATE TABLE `asiapay_ref` (
+//`id` int(11) NOT NULL AUTO_INCREMENT,
+//  `Ref` varchar(32) DEFAULT NULL,
+//  `Ord` varchar(255) DEFAULT NULL,
+//  `PayRef` varchar(32) DEFAULT NULL,
+//  `successcode` tinyint(2) DEFAULT NULL,
+//  `Amt` decimal(8,2) DEFAULT NULL,
+//  `Cur` varchar(3) DEFAULT NULL,
+//  `Holder` varchar(255) DEFAULT NULL,
+//  `AuthId` varchar(32) DEFAULT NULL,
+//  `AlertCode` varchar(255) DEFAULT NULL,
+//  `remark` text DEFAULT NULL,
+//  `eci` varchar(3) DEFAULT NULL,
+//  `payerAuth` varchar(2) DEFAULT NULL,
+//  `sourceIp` varchar(64) DEFAULT NULL,
+//  `ipCountry` varchar(3) DEFAULT NULL,
+//  `payMethod` varchar(16) DEFAULT NULL,
+//  `panFirst4` varchar(4) DEFAULT NULL,
+//  `panLast4` varchar(4) DEFAULT NULL,
+//  `cardIssuingCountry` varchar(3) DEFAULT NULL,
+//  `channelType` varchar(3) DEFAULT NULL,
+//  `prc` tinyint(2) DEFAULT NULL,
+//  `src` tinyint(2) DEFAULT NULL,
+//  `TxTime` datetime DEFAULT NULL,
+//  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+//  PRIMARY KEY (`id`),
+//  UNIQUE KEY `Ref` (`id`)
+//) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='asiapay datafeed';
+//SQLTEXT;
+
+$installer->run("DROP TABLE IF EXISTS `{$installer->getTable('asiapay/asiapay')}`;
+CREATE TABLE `{$installer->getTable('asiapay/asiapay')}` (
 `id` int(11) NOT NULL AUTO_INCREMENT,
   `Ref` varchar(32) DEFAULT NULL,
   `Ord` varchar(255) DEFAULT NULL,
@@ -31,10 +66,7 @@ CREATE TABLE `asiapay_ref` (
   `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `Ref` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='asiapay datafeed';
-SQLTEXT;
-
-$installer->run($sql);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='asiapay datafeed'");
 //demo 
 //Mage::getModel('core/url_rewrite')->setId(null);
 //demo 
